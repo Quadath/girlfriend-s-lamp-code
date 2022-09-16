8e4107bf6162a09bbf534fc5e40c0d283dada20a
Roman Savransky'y <91686101+Quadatz@users.noreply.github.com>
2022-09-16 17:29:26 +0200

    rainbow

--- sketch_aug29b.ino
+++ sketch_aug29b.ino
@@ -4,9 +4,7 @@
 
 #include <microLED.h>
 #include <IRremote.hpp>
-#include "FastLED.h"
 
-CRGB leds[NUMLEDS];
 
 const int button0 = 25;
 const int button1 = 69;
@@ -19,22 +17,26 @@ const int button7 = 7;
 const int button8 = 21;
 const int button9 = 9;
 
+const int starButton = 22;
+
 static float r = 0;
 static float g = 0;
-static float b = 10;
+static float b = 1;
 
+int mode = 1;
 
+microLED<NUMLEDS, STRIP_PIN, MLED_NO_CLOCK, LED_WS2818, ORDER_GRB, CLI_AVER> strip;
 void setup() {
   Serial.begin(9600);
   IrReceiver.begin(3, ENABLE_LED_FEEDBACK);
 
-  FastLED.addLeds<NEOPIXEL, STRIP_PIN>(leds, NUMLEDS);
-  FastLED.setBrightness(50);
+  
+  strip.setBrightness(255);
 
   for (int i = 0; i < NUMLEDS; i++) {
-    leds[i] = CRGB(r, g, b);
+    strip.set(i, mRGB(r, g, b));
   }
-  FastLED.show();
+  strip.show();
 }
 
 float lerp(float a, float b, float x)
@@ -46,41 +48,67 @@ void loop() {
 
   if (IrReceiver.decode()) {
     Serial.println(IrReceiver.decodedIRData.command);
+
     switch (IrReceiver.decodedIRData.command) {
-      case button1: {
+      case starButton: {
+        mode += 1;
+        if (mode == 3) {
+          mode = 1;
+        }
+      }
+    }
+    
+    if (mode == 1) { 
+       switch (IrReceiver.decodedIRData.command) {
+        case button1: {
           r += 2;
           if (r > 255) r = 255;
         } break;
-      case button4: {
-          r -= 2;
-          if (r < 0) r = 0;
-        } break;
-
-      case button2: {
-          g += 2;
-          if (g > 255) g = 255;
-        } break;
-      case button5: {
-          g -= 2;
-          if (g < 0) g = 0;
-        } break;
-
-      case button3: {
-          b += 2;
-          if (b > 255) b = 255;
-        } break;
-      case button6: {
-          b -= 2;
-          if (b < 0) b = 0;
-        } break;
-    }
-    delay(1);
-
-    for (int i = 0; i < NUMLEDS; i++) {
-      leds[i] = CRGB(r, g, b);
-    }
-    FastLED.show();
+        case button4: {
+            r -= 2;
+            if (r < 0) r = 0;
+          } break;
+  
+        case button2: {
+            g += 2;
+            if (g > 255) g = 255;
+          } break;
+        case button5: {
+            g -= 2;
+            if (g < 0) g = 0;
+          } break;
+  
+        case button3: {
+            b += 2;
+            if (b > 255) b = 255;
+          } break;
+        case button6: {
+            b -= 2;
+            if (b < 0) b = 0;
+        }
+      }
+      for (int i = 0; i < NUMLEDS; i++) {
+        strip.set(i, mRGB(r, g, b));
+      }
+    } 
     IrReceiver.resume();
   }
-
+  if (IrReceiver.isIdle()) {
+   
+    if (mode == 2) {
+      rainbow();
+      switch (IrReceiver.decodedIRData.command) {
+        
+      }
+    }
+  }
+  strip.show();
+  delay(16);
+}
+void rainbow() {
+  static byte counter = 0;
+  for (int i = 0; i < NUMLEDS; i++) {
+    strip.set(i, mWheel8(counter + i * 255 / NUMLEDS));   // counter смещает цвет
+  }
+  counter += 3;   // counter имеет тип byte и при достижении 255 сбросится в 0
 }
